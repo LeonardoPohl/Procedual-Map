564f1c4af5f5a66cdf12c04f0dfc100c0b887345
LeonardoPohl <pohly3@gmail.com>
2021-03-02 17:31:36 +0100

    Minor spelling errors leading to bugs

--- features/TerrainMap.py
+++ features/TerrainMap.py
@@ -3,11 +3,11 @@ import numpy as np
 from helper.utilities import offset_out_of_bounds, normalise
 from helper.noise import noise
 
-class terrain:
+class Terrain:
   def __init__(self, X:int = 100, Y:int = 100):
     self.X = X
     self.Y = Y
-    self.heigt_map = np.zeros((X,Y))
+    self.height_map = np.zeros((X,Y))
 
   def generate_height(self):
     print('Generating Height map...')
@@ -15,5 +15,5 @@ class terrain:
     self.height_map = np.array([sum(x) for x in zip(self.height_map, noise.generate_noise_Worley(self.X, self.Y, 30, 1))])
     print('Generating Perlin noise...')
     self.height_map = np.array([sum(x) for x in zip(self.height_map, noise.generate_noise_weird(self.X, self.Y))])
-    
-    self.heigt_map = normalise(self.height_map)
+
+    self.height_map = normalise(self.height_map)
--- features/humidityMap.py
+++ features/humidityMap.py
@@ -3,15 +3,15 @@ import numpy as np
 from helper.utilities import offset_out_of_bounds, dist, normalise
 from helper.noise import noise
 
-class humidity:
-  def __init__(self, X, Y, water):
-    self.X = X
-    self.Y = Y
+class Humidity:
+  def __init__(self, water):
     self.water = water
-    self.humidity_map = np.zeros(X, Y)
+    self.X = water.water_map.shape[0]
+    self.Y = water.water_map.shape[1]
+    self.humidity_map = np.zeros_like(water.water_map)
 
   def generate_humidity_complex(self):
-    self.humidity_map = np.zeros(self.X, self.Y)
+    self.humidity_map = np.zeros((self.X, self.Y))
     water_points = np.array(self.water.water_map) > 0
     a = 0.9
     max_dist = 50
@@ -36,7 +36,7 @@ class humidity:
       print(f'Row {x+1} out of {self.X}')
   
   def generate_humidity_random(self):
-    self.humidity_map = np.zeros(self.X, self.Y)
+    self.humidity_map = np.zeros((self.X, self.Y))
     print('Generating Worley noise...')
     self.humidity_map = np.array([sum(x) for x in zip(self.humidity_map, noise.generate_noise_Worley(self.X, self.Y, 30, 1))])
     print('Generating Perlin noise...')
@@ -45,4 +45,5 @@ class humidity:
     self.humidity_map = normalise(self.humidity_map)
   
   def generate_humidity_even(self, value:float = 0.5):
+    self.humidity_map = np.zeros((self.X, self.Y))
     self.humidity_map = np.full_like(self.humidity_map, value)
