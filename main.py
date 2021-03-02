1b33d9efca69ce27daed034994791fc9b7419ece
LeonardoPohl <pohly3@gmail.com>
2021-03-02 16:45:24 +0100

    Finished Reorginization

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from sys import argv
 from os import system
-
+from features import TerrainMap, WaterMap, HumidityMap
+from helper.visualization import Visulizer
 
 if __name__ == '__main__':
   system('color')
+  
+  X = 300
+  Y = 200
+  shadow_val = 50
 
-  open_menu()
+  terrain = TerrainMap.Terrain(X, Y)
+  terrain.generate_height()
+  visualizer = Visulizer(X, Y, terrain, None, None, shadow_val)
+  visualizer.draw_map(f'{X}x{Y}_Map_no_water.png')
 
-X = 400
-Y = 200
-shadow_val = 50
+  water = WaterMap.Water(terrain)
+  water.generate_rivers(10)
+  water.generate_lakes(10)
+  visualizer = Visulizer(X, Y, terrain, water, None, shadow_val)
+  visualizer.draw_map(f'{X}x{Y}_Map_no_humidity.png')
 
+  humidity = HumidityMap.Humidity(water)
+  humidity.generate_humidity_random()
 
-humidity_map = generate_noise_perlin()
-height_map, water_map = generate_height()
-draw_map(height_map, water_map, humidity_map)
+  visualizer = Visulizer(X, Y, terrain, water, humidity, shadow_val)
+  visualizer.draw_map(f'{X}x{Y}_Map_random_humidity.png')
 
-water_map = generate_rivers(1, height_map, water_map, humidity_map)
-draw_map(height_map, water_map, humidity_map)
-
-water_map = generate_lakes(10, height_map, water_map, humidity_map)
-draw_map(height_map, water_map, humidity_map)
-
-humidity_map = generate_humidity(height_map, water_map, humidity_map)
-draw_map(height_map, water_map, humidity_map)
+  for i in range(0,11,2):
+    humidity.generate_humidity_even(i/10)
+    visualizer = Visulizer(X, Y, terrain, water, humidity, shadow_val)
+    visualizer.draw_map(f'{X}x{Y}_Map_{i/10}_humidity.png')
